af42e410eaa0aa9e6a1e19655b50b0a80f703340
jackfengbiohazer <sfeng21@yahoo.com.au>
2020-01-01 16:06:23 +1100

    add Person Model

---
+++ testdotnetmvc/testdotnetmvc/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testdotnetmvc.Models
+{
+    public class Person
+    {
+        private int Pid { get; set; }
+        private string PersonName { get; set; }
+    }
+
+}
