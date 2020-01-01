61392f65af31b6413ac270d189e3b3367938b261
jackfengbiohazer <sfeng21@yahoo.com.au>
2020-01-01 16:02:55 +1100

    add Movie Model

---
+++ testdotnetmvc/testdotnetmvc/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(testdotnetmvc.Models.Movie))]
+
+namespace testdotnetmvc.Models
+{
+    public class Movie
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+
+        private int Id { get; set; }
+        private string Name { get; set; }
+    }
+}
